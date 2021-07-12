3b3f799b6dafe4efadfbd9884ffe61f507d2381c
judy222927529@gmail.com <s107213023@mial1.ncnu.edu.tw>
2021-07-13 01:08:18 +0800

    add temp pages & exam/homework sidebar

---
+++ src/views/admin/course/CourseList.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  CButton, CCard, CCardBody,
+  CRow, CCol, CContainer,
+  CCreateElement, CSidebarNavItem,
+} from '@coreui/react';
+
+const HomeWorkItemLink = (props) => {
+  return <CButton type="button" component="a" {...props} />;
+}
+
+const HomeWorkSideBar = [
+  {
+    _tag: 'CSidebarNavItem',
+    name: '測驗管理',
+    to: '/course/examinfo',
+    icon: 'cil-code',
+    badge: {
+      color: 'info',
+    }
+  },
+  {
+    _tag: 'CSidebarNavItem',
+    name: '作業管理',
+    to: '/course/homeworklist',
+    icon: 'cil-keyboard',
+    badge: {
+      color: 'info',
+    }
+  }
+]
+
+const Item = (props) => {
+  const x = props.item;
+  const dispatch = useDispatch();
+  const backToCourseList = () => {
+    dispatch({ type: 'set', customNavBar: null });
+  };
+  const goToHomeworkList = () => {
+    dispatch({
+      type: 'set', customNavBar: () => {
+        return (
+          <>
+            <CSidebarNavItem to='/course/courselist' onClick={backToCourseList} name='返回' icon='cil-arrowCircleLeft' />
+            <CCreateElement
+              items={HomeWorkSideBar}
+              components={{
+                CSidebarNavItem
+              }} />
+
+
+          </>
+        )
+      }
+    });
+  };
+  return (
+    <>
+      <CContainer md={10} xs={12}>
+        <CCard id={x.id}>
+          <CRow>
+            <CCol md={8} xs={6}>
+              <h2>{x.courseName}</h2>
+            </CCol>
+            <CCol>
+              <CCol className="d-none d-md-block">
+                <HomeWorkItemLink color="primary" className="float-right"
+                  onClick={goToHomeworkList} href={`#course/homeworklist`}>
+                  進入
+                </HomeWorkItemLink>
+              </CCol>
+            </CCol>
+          </CRow>
+        </CCard>
+      </CContainer>
+    </>
+  );
+}
+
+const CourseList = () => {
+  const [allCourse, setAllCourse] = useState([])
+  useEffect(() => {
+    setAllCourse([
+      {
+        "id": 0,
+        "courseName": "程式設計"
+      },
+      {
+        "id": 1,
+        "courseName": "Hello World"
+      }
+    ]);
+  }, []);
+  return (
+    <>
+      <CCard>
+        <CCardBody>
+          <CRow>
+            {
+              allCourse.map((x) => (
+                <Item key={x.id} item={x} />
+              ))
+            }
+          </CRow>
+        </CCardBody>
+      </CCard>
+    </>
+  )
+}
+export default CourseList;
---
+++ src/views/admin/exam/ExamInfo.js
@@ -0,0 +1,28 @@
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CRow,
+  CCol,
+} from '@coreui/react'
+const ExamInfo = () => {
+  return (
+    <>
+      <CCard>
+        <CCardBody>
+          <CRow>
+            <CCol sm="5">
+              <h4 id="traffic" className="card-title mb-0">Traffic</h4>
+              <div className="small text-muted">November 2017</div>
+            </CCol>
+            <CCol sm="7" className="d-none d-md-block">
+              <CButton color="primary" className="float-right">
+              </CButton>
+            </CCol>
+          </CRow>
+        </CCardBody>
+      </CCard>
+    </>
+  )
+}
+export default ExamInfo;
