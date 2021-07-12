de5ee556dadbefea6e31446100a6022edc00954d
judy222927529@gmail.com <s107213023@mial1.ncnu.edu.tw>
2021-07-13 01:07:14 +0800

    add Course routes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,9 +1,15 @@
 import React from 'react';
 const Dashboard = React.lazy(() => import('../views/admin/dashboard/Dashboard'));
+const CourseList = React.lazy(() => import('../views/admin/course/CourseList'));
+const HomeWorkList = React.lazy(() => import('../views/admin/homework/HomeWorkList'));
+const ExamInfo = React.lazy(() => import('../views/admin/exam/ExamInfo'));
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
+  { path: '/course/courselist', name: 'CourseList', component: CourseList },
+  { path: '/course/homeworklist', name: 'HomeWorkList', component: HomeWorkList },
+  { path: '/course/examinfo', name: 'ExamInfo', component: ExamInfo },
 ];
 
 export default routes;
