4296b5f931b7a6c12ee551601ad46dc6f5b083c4
judy222927529@gmail.com <s107213023@mial1.ncnu.edu.tw>
2021-10-16 16:29:48 +0800

    add Exam routes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -2,13 +2,19 @@ import React from 'react';
 const Dashboard = React.lazy(() => import('../views/admin/dashboard/Dashboard'));
 const CourseList = React.lazy(() => import('../views/admin/course/CourseList'));
 const HomeWorkList = React.lazy(() => import('../views/admin/homework/HomeWorkList'));
+const ExamList = React.lazy(() => import('../views/admin/exam/ExamList'));
 const ExamInfo = React.lazy(() => import('../views/admin/exam/ExamInfo'));
+const CreateExam = React.lazy(() => import('../views/admin/exam/CreateExam'));
+const CreateQuestion = React.lazy(() => import('../views/admin/exam/CreateQuestion'));
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   { path: '/course/courselist', name: 'CourseList', component: CourseList },
   { path: '/course/homeworklist', name: 'HomeWorkList', component: HomeWorkList },
+  { path: '/course/examlist', name: 'ExamList', component: ExamList },
+  { path: '/course/createexam', name: 'CreateExam', component: CreateExam },
+  { path: '/course/createquestion', name: 'CreateQuestion', component: CreateQuestion },
   { path: '/course/examinfo', name: 'ExamInfo', component: ExamInfo },
 ];
 
